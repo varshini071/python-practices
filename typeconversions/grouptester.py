63f303604c8fe559351f3022c9442963e17ce493
varshini071 <varshinivangala703@gmail.com>
2026-09-26 02:23:22 +0530

    Added grouptester program

---
+++ typeconversions/grouptester.py
@@ -0,0 +1,9 @@
+#Write a Python program that checks whether a specified value is contained within a group of values.
+
+m=[1, 5, 8, 3]
+x = int(input("enter the value: "))
+
+if x in m:
+    print(True)
+else:
+    print(False)
